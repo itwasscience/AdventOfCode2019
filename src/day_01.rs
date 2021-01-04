bf786bb489923ae0b12478cb3645952f1d445277
Zoey <zoey.fahner@gmail.com>
2021-01-03 21:30:22 -0500

    Day 1 Part 1

--- src/day_01.rs
+++ src/day_01.rs
@@ -2,7 +2,12 @@ fn calc_module_fuel(mass: i64) -> i64 {
     ((mass as f64 / 3.0).floor() - 2.0) as i64
 }
 
-pub fn part_1() {}
+pub fn part_1(modules: Vec<i64>) -> () {
+    let fuel: i64 = modules.iter().map(|&x| calc_module_fuel(x)).sum();
+    println!("Day 01, Part 1: {}", fuel);
+}
+
+pub fn part_2(modules: Vec<i64>) -> () {}
 
 #[cfg(test)]
 mod day_01_tests {
