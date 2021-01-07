fb63ce589c7dc20c3f415812f3ba2dd27d8be762
Zoey <zoey.fahner@gmail.com>
2021-01-07 08:15:34 -0500

    More refactors to intcode for day2. Much cleaner!

--- src/day_02.rs
+++ src/day_02.rs
@@ -1,47 +1,31 @@
-use crate::intcode;
+use crate::intcode::intcode::Intcode;
 
-fn fix_crash(mut memory: Vec<isize>) -> Vec<isize> {
-    memory[1] = 12;
-    memory[2] = 2;
-    memory
+pub fn part_1(program: Vec<isize>) -> String {
+    let mut intcode = Intcode::new();
+    intcode.load_program(program);
+    // Fix Crash
+    intcode.poke(1, 12);
+    intcode.poke(2, 2);
+    intcode.run();
+    format!("Part 1: {}", intcode.peek(0)).to_string()
 }
 
-fn attempt_run(mut memory: Vec<isize>, x: isize, y: isize) -> Vec<isize> {
-    memory[1] = x;
-    memory[2] = y;
-    intcode::run(memory, 0)
-}
-
-fn brute_force(memory: Vec<isize>) -> (isize, isize) {
-    for x in 0..100 {
-        for y in 0..100 {
-            let copy = memory.clone();
-            let value = *attempt_run(copy, x, y).iter().nth(0).unwrap();
-            if value == 19_690_720 {
-                return (x, y);
+pub fn part_2(program: Vec<isize>) -> String {
+    let mut x: i16 = 0;
+    let mut y: i16 = 0;
+    for i in 0..100 {
+        for j in 0..100 {
+            let clone = program.clone();
+            let mut intcode = Intcode::new();
+            intcode.load_program(clone);
+            intcode.poke(1, i); // Noun
+            intcode.poke(2, j); // Verb
+            intcode.run();
+            if intcode.peek(0) == 19_690_720 {
+                x = i as i16;
+                y = j as i16;
             }
         }
     }
-    panic!("Match not found for combination of x and y, Aborting!");
-}
-
-pub fn part_1(mut memory: Vec<isize>) -> String {
-    memory = fix_crash(memory);
-    let result = *intcode::run(memory, 0).iter().nth(0).unwrap();
-    format!("Part 1: {}", result).to_string()
-}
-
-pub fn part_2(memory: Vec<isize>) -> String {
-    let (x, y) = brute_force(memory);
     format!("Part 2: {}", 100 * x + y).to_string()
-}
-
-#[cfg(test)]
-mod day_02_tests {
-    #[test]
-    fn part_1() {
-        // Moved to intcode module
-    }
-    #[test]
-    fn part_2() {}
 }
--- src/intcode.rs
+++ src/intcode.rs
@@ -1,31 +1,73 @@
-pub fn run(mut memory: Vec<isize>, ip: usize) -> Vec<isize> {
-    loop {
-        match memory.iter().nth(ip).unwrap() {
-            99 => return memory,
-            1 => {
-                let a = *memory.iter().nth(ip + 1).unwrap();
-                let b = *memory.iter().nth(ip + 2).unwrap();
-                let dst = *memory.iter().nth(ip + 3).unwrap();
-                if dst >= 0 && a >= 0 && b >= 0 {
-                    memory[dst as usize] = memory[a as usize] + memory[b as usize];
-                } else {
-                    panic!("Memory access at negative index is not allowed!")
-                }
-                return run(memory, ip + 4);
-            }
-            2 => {
-                let a = *memory.iter().nth(ip + 1).unwrap();
-                let b = *memory.iter().nth(ip + 2).unwrap();
-                let dst = *memory.iter().nth(ip + 3).unwrap();
-                if dst >= 0 && a >= 0 && b >= 0 {
-                    memory[dst as usize] = memory[a as usize] * memory[b as usize];
-                } else {
-                    panic!("Memory access at negative index is not allowed!")
-                }
-                return run(memory, ip + 4);
-            }
+pub mod intcode {
+    #[derive(Debug)]
+    pub struct Intcode {
+        memory: Vec<isize>, // Special memory that can hold negative values
+        ip: usize,          // Instruction Pointer
+        input: isize,       // External Port
+        output: isize,      // External Port
+        halt: bool,         // Halting Flag - Opcode 99
+    }
 
-            _ => return memory,
+    impl Intcode {
+        pub fn new() -> Intcode {
+            Intcode {
+                memory: Vec::new(),
+                ip: 0,
+                input: 0,
+                output: 0,
+                halt: false,
+            }
+        }
+        pub fn load_program(&mut self, program: Vec<isize>) {
+            self.memory = program.clone()
+        }
+        pub fn peek(&mut self, memory_addr: usize) -> isize {
+            *self.memory.iter().nth(memory_addr).unwrap()
+        }
+        pub fn poke(&mut self, memory_addr: usize, value: isize) {
+            std::mem::swap(&mut self.memory[memory_addr], &mut value.clone());
+        }
+        pub fn step(&mut self) {
+            // println!("{:?}", self);
+            match self.memory.iter().nth(self.ip).unwrap() {
+                99 => {
+                    self.halt = true;
+                }
+                1 => {
+                    let a = *self.memory.iter().nth(self.ip + 1).unwrap();
+                    let b = *self.memory.iter().nth(self.ip + 2).unwrap();
+                    let dst = *self.memory.iter().nth(self.ip + 3).unwrap();
+                    if dst >= 0 && a >= 0 && b >= 0 {
+                        self.memory[dst as usize] =
+                            self.memory[a as usize] + self.memory[b as usize];
+                    } else {
+                        panic!("self.memory access at negative index is not allowed!")
+                    }
+                    self.ip += 4;
+                }
+                2 => {
+                    let a = *self.memory.iter().nth(self.ip + 1).unwrap();
+                    let b = *self.memory.iter().nth(self.ip + 2).unwrap();
+                    let dst = *self.memory.iter().nth(self.ip + 3).unwrap();
+                    if dst >= 0 && a >= 0 && b >= 0 {
+                        self.memory[dst as usize] =
+                            self.memory[a as usize] * self.memory[b as usize];
+                    } else {
+                        panic!("self.memory access at negative index is not allowed!")
+                    }
+                    self.ip += 4;
+                }
+                _ => (),
+            }
+        }
+        pub fn run(&mut self) {
+            loop {
+                if self.halt {
+                    return;
+                } else {
+                    self.step();
+                }
+            }
         }
     }
 }
@@ -35,12 +77,43 @@ mod intcode_tests {
     use super::*;
 
     #[test]
-    fn day_02_opcodes() {
-        assert_eq!(*run(vec![1, 0, 0, 0, 99], 0).iter().nth(0).unwrap(), 2);
-        assert_eq!(*run(vec![2, 3, 0, 3, 99], 0).iter().nth(3).unwrap(), 6);
-        assert_eq!(
-            *run(vec![2, 4, 4, 5, 99, 0], 0).iter().nth(5).unwrap(),
-            9801
-        );
+    fn day_02_opcodes_test_addition() {
+        let mut intcode = intcode::Intcode::new();
+        intcode.load_program(vec![1, 0, 0, 0, 99]);
+        intcode.run();
+        assert_eq!(intcode.peek(0), 2);
+    }
+    #[test]
+    fn day_02_opcodes_test_multiplication() {
+        let mut intcode = intcode::Intcode::new();
+        intcode.load_program(vec![2, 3, 0, 3, 99]);
+        intcode.run();
+        assert_eq!(intcode.peek(3), 6);
+    }
+    #[test]
+    fn day_02_opcodes_small_program() {
+        let mut intcode = intcode::Intcode::new();
+        intcode.load_program(vec![2, 4, 4, 5, 99, 0]);
+        intcode.run();
+        assert_eq!(intcode.peek(5), 9801);
+    }
+    #[test]
+    fn test_peek() {
+        let mut intcode = intcode::Intcode::new();
+        intcode.load_program(vec![0, 1, 2]);
+        assert_eq!(intcode.peek(0), 0);
+        assert_eq!(intcode.peek(1), 1);
+        assert_eq!(intcode.peek(2), 2);
+    }
+    #[test]
+    fn test_poke() {
+        let mut intcode = intcode::Intcode::new();
+        intcode.load_program(vec![0, 0, 0]);
+        intcode.poke(0, 0);
+        intcode.poke(1, 1);
+        intcode.poke(2, 2);
+        assert_eq!(intcode.peek(0), 0);
+        assert_eq!(intcode.peek(1), 1);
+        assert_eq!(intcode.peek(2), 2);
     }
 }
